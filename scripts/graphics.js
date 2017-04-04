0f3e2f67fd614ca90385e029f5996f780e58d919
Ben Phillips <bennyp95@gmail.com>
2017-04-04 09:10:32 -0600

    Added the graphics js file and menu js files

---
+++ scripts/graphics.js
@@ -0,0 +1,3 @@
+Game.graphics = function(){
+    
+}()
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,25 @@
+Game.screens = {
 
+}
+
+// game contains gameloop, update, and render. Runs the heart of the game.
+Game.game = function(){
+    function init(){
+
+    }
+
+    function gameLoop(elapsedTime){
+        update(elapsedTime);
+        render();
+
+        requestAnimationFrame(gameLoop);
+    }
+
+    function update(elapsedTime){
+
+    }
+
+    function render(){
+
+    }
+}()
