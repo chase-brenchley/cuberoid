7ad8af503d1278828394e6f19cc62fca481cf034
Ben Phillips <bennyp95@gmail.com>
2017-04-07 13:07:11 -0600

    Added a comment in object.js

--- scripts/object.js
+++ scripts/object.js
@@ -1,4 +1,5 @@
-class object{
+class rect{
+    // the x,y value of the spec is the center of the object
     constructor(spec){
         this.height = spec.height;
         this.width = spec.width;
