7e18c49c6c421c987aef9624476edd98334541dd
chase-brenchley <chase.brenchley@gmail.com>
2017-04-13 13:32:29 -0600

    git commit 'stage1.js'

---
+++ scripts/stages/stage1.js
@@ -0,0 +1,12 @@
+Gamepad.stage1 = function() {
+    // Spawn area. There's only a floor and a door to exit
+    var width, height
+
+    function draw() {
+        Gam
+    }
+
+    return {
+        draw:draw,
+    }
+}
