f8a8cefba13b973450132603ebad607d7ee947fe
chase-brenchley <chase.brenchley@gmail.com>
2017-04-13 14:32:44 -0600

    reset to default controls functional

--- scripts/controls.js
+++ scripts/controls.js
@@ -2,28 +2,45 @@ Game.controls = function(){
     controls = {};
 
     function changeControls(control){
-    // prompt user to choose a new button or press esc to cancel
-    button = document.getElementById(control);
-    document.getElementById('page-mainmenu').focus(); // Removes focus from button so space can be set as a control
-    button.blur(); // Look above
-    current = button.innerText;
-    button.innerText = "Press any key..."
-    var keynum;
+        // prompt user to choose a new button or press esc to cancel
+        button = document.getElementById(control);
+        document.getElementById('page-mainmenu').focus(); // Removes focus from button so space can be set as a control
+        button.blur(); // Look above
+        current = button.innerText;
+        button.innerText = "Press any key..."
+        var keynum;
 
-    document.onkeydown = function(evt) {
-        evt = evt || window.event;
-        var charCode = evt.keyCode || evt.which;
-        if (27 == charCode) {
-            button.innerText = current;
-            return;
+        document.onkeydown = function(evt) {
+            evt = evt || window.event;
+            var charCode = evt.keyCode || evt.which;
+            if (27 == charCode) {
+                button.innerText = current;
+                return;
+            }
+            var charStr = String.fromCharCode(charCode);
+            button.innerText = charStr;
+            // alert(charStr);
+        };
+        // update control dictionary
+        controls[control] = button.innerText;
+    }   
+
+    function reset(){
+        // Reset controls to default
+        document.getElementById('jump').innerText = "Space";
+        document.getElementById('shoot').innerText = "E";
+        document.getElementById('left').innerText = "s";
+        document.getElementById('right').innerText = "f";
+        document.getElementById('up').innerText = "e";
+        document.getElementById('down').innerText = "d";
+
+        // update controls object
+        controlButtons = document.getElementsByClassName("control-button");
+        for (var i = 0; i < controlButtons.length; i++) {
+            var element = controlButtons[i];
+			controls[element.id] = element.innerText;            
         }
-        var charStr = String.fromCharCode(charCode);
-        button.innerText = charStr;
-        // alert(charStr);
-    };
-    // update control dictionary
-    controls[control] = button.innerText;
-}
+    }
 
     function init(){
         controlButtons = document.getElementsByClassName("control-button");
@@ -37,5 +54,6 @@ Game.controls = function(){
         init:init,
         controls:controls,
         changeControls:changeControls,
+        reset: reset,
     }
 }();
