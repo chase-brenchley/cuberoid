1542a98f614c73ff431e219cfe027003e1f49f6f
Ben Phillips <Ben.Phillips@sdl.usu.edu>
2017-04-13 19:40:28 -0600

    Created collision detection in the new physics.js file

--- scripts/main.js
+++ scripts/main.js
@@ -7,12 +7,13 @@ Game.screens = {
 Game.game = function(){
     var floor = document.getElementById('canvas-main').height - 20;
     var curTime, prevTime;
-    var seamus, graphics;
+    var seamus, graphics, physics;
 
     function init(){
         curTime = prevTime = performance.now();
-        seamus = Game.seamus; 
+        seamus = Game.seamus.generateSeamus(); 
         graphics = Game.graphics;
+        physics = Game.physics;
         graphics.init();
         seamus.init();
         requestAnimationFrame(gameLoop);
@@ -30,78 +31,15 @@ Game.game = function(){
 
     function update(elapsedTime){
         seamus.update(elapsedTime)
+        if(physics.collision(seamus, {x: 0, y: 400, width: 1000, height: 50, color: 'blue'})){
+            console.log("you did it!")
+        }
     }
 
     function render(){
         graphics.clear();
+        graphics.drawRect({x: 0, y: 400, width: 1000, height: 50, color: 'blue'});        
         seamus.draw();
-    }
-
-    // Takes two objects and detects if they are colliding. Assumes the first parameter is a moving object, and the other is 
-    // treated as being stationary, so when position is corrected to make sure they are not superimposed on each other the 
-    // moving object's position will be changed. Returns true if they are colliding, else returns false.
-    function collision(movingObj, obj){
-        var movingRightSide = movingObj.x + .5 * movingObj.width, movingLeftSide = movingObj.x - .5 * movingObj.width;
-        var movingTop = movingObj.y - .5 * movingObj.height, movingBottom = movingObj.y + .5 * movingObj.height;
-
-        var stationaryLeftside = obj.x - .5 * obj.width, stationaryRightSide = obj.x + .5 * obj.width;
-        var stationaryTop = obj.y - .5 * obj.height, stationaryBottom = obj.y + .5 * obj.height;
-
-        var collisionOverlap = 1000;
-
-        var collisionSide;
-
-        // Detect side collisions. First test that they have some vertical overlap
-        if(){
-            // Detect case where moving obj hits left side of stationary obj
-            if(movingRightSide > stationaryLeftside && movingRightSide < stationaryRightSide){
-                //movingObj.x = stationaryLeftside - .5 * movingObj.width;
-                if(stationaryLeftside - movingRightSide < collisionOverlap){
-                    collisionOverlap = stationaryLeftside - movingRightSide;
-                    collisionSide = "left";
-                }
-            }
-
-            // Detect case where moving obj hits right side of stationary obj
-            if(movingLeftSide < stationaryRightSide && movingLeftSide > stationaryLeftside){
-                //movingObj.x = stationaryRightSide + .5 * movingObj.width; 
-                if(movingLeftSide - stationaryRightSide < collisionOverlap){
-                    collisionOverlap = stationaryLeftside - movingRightSide;
-                    collisionSide = "right";
-                }
-            }
-        }
-
-        // Detect Vertical collision. First check that there is horizontal overlap
-        if((movingLeftSide < stationaryBottom && movingTop > stationaryTop) || (movingBottom < stationaryBottom && movingBottom > stationaryTop)){
-            // Detect case where moving obj hits left side of stationary obj
-            if(movingRightSide > stationaryLeftside && movingRightSide < stationaryRightSide){
-                //movingObj.x = stationaryLeftside - .5 * movingObj.width;
-                if(stationaryLeftside - movingRightSide < collisionOverlap){
-                    collisionOverlap = stationaryLeftside - movingRightSide;
-                    collisionSide = "left";
-                }
-            }
-
-            // Detect case where moving obj hits right side of stationary obj
-            if(movingLeftSide < stationaryRightSide && movingLeftSide > stationaryLeftside){
-                //movingObj.x = stationaryRightSide + .5 * movingObj.width; 
-                if(movingLeftSide - stationaryRightSide < collisionOverlap){
-                    collisionOverlap = stationaryLeftside - movingRightSide;
-                    collisionSide = "right";
-                }
-            }
-        }
-        
-    }
-
-    function verticalOverlap(obj, obj2){
-        var objTop = obj.y - .5 * obj.height, objBottom = obj.y + .5 * obj.height;
-        var obj2Top = obj2.y - .5 * obj2.height, obj2Bottom = obj2.y + .5 * obj2.height;
-
-        if((objTop <= obj2Bottom && objTop >= obj2Top) || (objBottom < obj2Bottom && objBottom > obj2Top)){
-            return true;
-        }
     }
 
     return{
