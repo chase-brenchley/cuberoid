5299b078effb8c7ba4b5251a661016803a98ec22
Chase Brenchley <chase.brenchley@sdl.usu.edu>
2017-04-23 17:41:26 -0600

    changes to stage2

--- scripts/stages/stage2.js
+++ scripts/stages/stage2.js
@@ -13,7 +13,9 @@ Game.stage2 = function() {
         {x: 1-.02/2, y: .85-.3/2-.2/2, width: .02, height: .2, color: "clear", nextStage: Game.stage1}, //door
         {x: .5, y: 0+.01/2, width:1 , height: .01 , color: "grey"}, //ceiling
         {x: 1-.015/2, y: .5/2, width: .015, height: 1-(.3+.2), color: "grey"}, //right wall
-        {x: .95+.05/2, y: .5, width: .05, height: .035, color: "grey"} // door overhang
+        {x: .95+.05/2, y: .5, width: .05, height: .035, color: "grey"}, // door overhang
+        // Ceiling
+        {x: .5, y: .5,width: 1, height: .05, color:"grey"},
     ]
 
     // var Constants = {
@@ -31,34 +33,6 @@ Game.stage2 = function() {
         for (i = 0; i < stage.length; i++){
             Game.graphics.drawRect(stage[i]);
         }
-
-        var image = new Image(); //h: 64 w: 31 R: 2.06
-        image.src = "assets/textures/closeddoorleft.png";
-        console.log(image.width)
-        console.log(image.height)
-        width = .045;
-        height = width * 4;
-        height = .185;
-        width = height / 4;
-
-        Game.graphics.drawImage({
-            image: image,
-            dx: 1-width/2,
-            dy: .608,
-            dWidth: width,
-            dHeight: height
-        });
-
-        var texture = new Image();
-        texture.src = "assets/textures/floor.jpg";
-
-        Game.graphics.drawImage({
-            image: texture,
-            dx: .5,
-            dy: .75,
-            dWidth: .1/1.7,
-            dHeight: .1,
-        })
     }
 
     return {
