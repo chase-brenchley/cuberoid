5bbe32d8e846224e7a30daddb554a26b48eb1002
chase-brenchley <chase.brenchley@gmail.com>
2017-04-24 13:17:11 -0600

    commit before leaving

--- scripts/main.js
+++ scripts/main.js
@@ -22,7 +22,7 @@ Game.game = (function(controls){
         graphics = Game.graphics;
         seamus = Game.seamus.generateSeamus(); 
         physics = Game.physics;
-        currentStage = Game.stage2;
+        currentStage = Game.stage1;
         
         physics.init();
         graphics.init();
