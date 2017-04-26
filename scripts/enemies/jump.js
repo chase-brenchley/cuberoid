d2b8a086c2b746fcac255038bab851587810dbcd
chase-brenchley <chase.brenchley@gmail.com>
2017-04-25 22:36:38 -0600

    removed fluff

--- scripts/enemies/jump.js
+++ scripts/enemies/jump.js
@@ -5,20 +5,11 @@ Game.enemies.bossJump = function(spec){
         that.health = 500;
         that.yVelocity = -1.5;
         that.timeUntilNextJump = 2000; // jumps every 2000 ms
-        // that.position;
-        // that.position = {};
-        // var direction, leftLimit, rightLimit;
-        
-        // Generates a basic enemy
-        // Takes in where the enemy starts and left and right limits
-        // that.init = function(spec){
-            var startLocation = spec.startLocation;
-            that.leftLimit = spec.leftLimit;
-            that.rightLimit = spec.rightLimit;
-            // randomly choose a direction to start moving
-            that.direction = Math.random() > .5 ? that.moveSpeed : -that.moveSpeed;
-            that.position = spec.startLocation;
-        // }();
+        that.leftLimit = spec.leftLimit;
+        that.rightLimit = spec.rightLimit;
+        that.direction = Math.random() > .5 ? that.moveSpeed : -that.moveSpeed;
+        that.position = spec.startLocation;
+
 
         that.draw = function(){
             Game.graphics.drawCornerRect({
@@ -33,17 +24,10 @@ Game.enemies.bossJump = function(spec){
         that.updatePosition = function(time){
             that.timeUntilNextJump -= time;
 
-            console.log(that.timeUntilNextJump);
-
             if (that.timeUntilNextJump <= 0) {
                 that.timeUntilNextJump = 2000;
                 that.yVelocity = -1.4;
-                that.position.y += that.yVelocity * time/1000;                
-
-                // if (that.timeUntilNextJump <= -1280) {
-                //     that.timeUntilNextJump = 2000;
-                //     that.yVelocity = -1.5
-                // }
+                that.position.y += that.yVelocity * time/1000;
             } 
             else if(that.position.y < 1-(.05+.17*2)) {
                 that.yVelocity += 3 * time/1000;
@@ -52,7 +36,8 @@ Game.enemies.bossJump = function(spec){
                 if(that.position.x + that.direction > that.rightLimit) that.direction = -that.moveSpeed;
                 else if(that.position.x - that.direction < that.leftLimit) that.direction = that.moveSpeed;
                 that.position.x += that.direction;
-            } else if(that.position.y > 1-(.05+.17*2)) that.position.y = 1-(.05+.17*2);
+            } 
+            else if(that.position.y > 1-(.05+.17*2)) that.position.y = 1-(.05+.17*2);
         }
 
         that.updateState = function(){
